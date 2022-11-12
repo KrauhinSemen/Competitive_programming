efc06eecdcf0b2bc19f9aaafbe06c33b976c7f7e
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2022-11-12 18:11:31 +0500

    Add files via upload

---
+++ Exp_2/Exp_2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Exp_2
+{
+    class Program
+    {
+        public interface IMultiLock
+        {
+            public IDisposable AcquireLock(params string[] keys);
+        }
+
+        class MultiLockStorage : IDisposable
+        {
+            public List<string> keys;
+            public MultiLock origin;
+            public object locker = new object();
+
+            public MultiLockStorage(List<string> keys_, MultiLock origin_)
+            {
+                lock (locker)
+                {
+                    keys = keys_;
+                    origin = origin_;
+                }
+            }
+
+            public void Dispose()
+            {
+                lock (locker)
+                {
+                    foreach (var key in keys)
+                        origin.keys.Remove(key);
+                }
+            }
+        }
+
+        class MultiLock : IMultiLock
+        {
+            public List<string> keys = new List<string>();
+            public object locker = new object();
+
+            public MultiLock(params string[] keys_)
+            {
+                lock (locker)
+                {
+                    foreach (var key in keys_)
+                        keys.Add(key);
+                }
+            }
+
+            public IDisposable AcquireLock(params string[] keys_)
+            {
+                while (true)
+                {
+                    lock (locker)
+                    {
+                        var tempStorage = new List<string>();
+                        var wasCoincident = false;
+                        foreach (var key in keys_)
+                        {
+                            if (keys.Contains(key))
+                            {
+                                wasCoincident = true;
+                                break;
+                            }
+                            tempStorage.Add(key);
+                        }
+                        if (!wasCoincident) 
+                            return new MultiLockStorage(tempStorage, this);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
