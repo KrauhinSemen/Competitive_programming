d8c9faa687a2d6a2ca83eeb8e9df91c41ceb4f6c
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2022-11-14 19:04:40 +0500

    fix_3

--- Exp_2/Exp_2/Program.cs
+++ Exp_2/Exp_2/Program.cs
@@ -38,6 +38,7 @@ namespace Exp_2
 
         public class MultiLock : IMultiLock
         {
+            public List<string> keyOptions = new List<string>(); // Все доступные варианты ключа
             public List<string> keys = new List<string>();  // Место хранения ключей
             public object locker = new object();  // "Заглушка" для lock
 
@@ -46,7 +47,7 @@ namespace Exp_2
                 lock (locker)
                 {
                     foreach (var key in keys_)  // Передача ключей классу
-                        keys.Add(key);
+                        keyOptions.Add(key);
                 }
             }
 
@@ -60,6 +61,8 @@ namespace Exp_2
                         var wasCoincident = false;
                         foreach (var key in keys_)
                         {
+                            if (!keyOptions.Contains(key))
+                                throw new ArgumentException("Выход за предел доступных для блокирования ключей");
                             if (keys.Contains(key))
                             {
                                 wasCoincident = true;
@@ -76,17 +79,45 @@ namespace Exp_2
                 }
             }
         }
-        
+
         static void Main(string[] args)
         {
             /*
             Небольшой тест
-
+            
             var y = new MultiLock("1k", "2k", "3k");
-            var t1 = y.AcquireLock("4k", "5k");
+            // var t1 = y.AcquireLock("4k", "5k"); // Ошибка 
+            // var t1 = y.AcquireLock("3k", "4k");  // Ошибка
+            var t1 = new Thread(() => 
+            {
+                var a = y.AcquireLock("1k", "2k", "3k");
+                Console.WriteLine("Поток 1 засыпает");
+                Thread.Sleep(2000);
+                Console.WriteLine("Поток 1 проснулся");
+                a.Dispose();
+            });
+            var t2 = new Thread(() =>
+            {
+                var b = y.AcquireLock("3k", "2k");
+                Console.WriteLine("Поток 2 засыпает");
+                Thread.Sleep(2000);
+                Console.WriteLine("Поток 2 проснулся");
+                b.Dispose();
+            });
+            var t3 = new Thread(() =>
+            {
+                var c = y.AcquireLock("0k", "3k");
+                Console.WriteLine("Поток 3 засыпает");
+                Thread.Sleep(2000);
+                Console.WriteLine("Поток 3 проснулся");
+                c.Dispose();
+            });
+            t1.Start();
+            t2.Start();
+            // t3.Start(); // Ошибка
             Console.WriteLine(y.keys);
-            t1.Dispose();
-            Console.WriteLine(y.keys);
+            Thread.Sleep(5000);
+            Console.WriteLine("End");
             */
         }
     }
